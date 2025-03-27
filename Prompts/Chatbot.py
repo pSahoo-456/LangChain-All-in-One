6d2734df256f4f533823f4316ecdaaf844d1f77e
pSahoo-456 <prakash2004sahoo@gmail.com>
2025-03-27 13:42:40 +0530

    Mesasges added and integrated it with the chatbot

--- Prompts/Chatbot.py
+++ Prompts/Chatbot.py
@@ -1,19 +1,22 @@
 from langchain_openai import ChatOpenAI
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
 from dotenv import load_dotenv
 
 load_dotenv()
 
-model=ChatOpenAI()
+model = ChatOpenAI()
 
-chat_history=[]
-
+chat_history = [
+    SystemMessage(content='You are a helpful AI assistant')
+]
 
 while True:
-    user_input=input("You:")
-    chat_history.append(user_input)
-    if user_input.lower()=='exit':
+    user_input = input('You: ')
+    chat_history.append(HumanMessage(content=user_input))
+    if user_input == 'exit':
         break
-    result=model.invoke(chat_history)
-    chat_history.append(result.content)
-    print("Chatbot:",result.content)
+    result = model.invoke(chat_history)
+    chat_history.append(AIMessage(content=result.content))
+    print("AI: ",result.content)
+
 print(chat_history)
